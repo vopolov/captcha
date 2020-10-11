3f0d9567e33d895b73cc334e8aaf456afb333975
Vladimir Popov <33720352+vopolov@users.noreply.github.com>
2020-10-11 22:40:44 +0300

    training with fixed size model; added tensorboard writer

--- train.py
+++ train.py
@@ -7,10 +7,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import Resize, ToTensor, Normalize, Compose
 
 from data import CaptchaDataset
-from model import CaptchaModel
+from model import CaptchaModel, CaptchaModelFixedSize
 from util import naive_ctc_decode
 
 
@@ -37,38 +38,44 @@ def run_one_epoch(phase, dataloader, model, criterion, optimizer, device):
 
             output = model(images)
 
-            batch = output.shape[1]
-            width = output.shape[0]
-            input_lens = torch.full(size=(batch,), fill_value=width, dtype=torch.long, device=device)
-            label_size = labels.shape[1]
-            label_lens = torch.full(size=(batch,), fill_value=label_size, dtype=torch.long, device=device)
-            loss = criterion(output, labels, input_lens, label_lens)
+            # batch = output.shape[1]
+            # width = output.shape[0]
+            # input_lens = torch.full(size=(batch,), fill_value=width, dtype=torch.long, device=device)
+            # label_size = labels.shape[1]
+            # label_lens = torch.full(size=(batch,), fill_value=label_size, dtype=torch.long, device=device)
+            # loss = criterion(output, labels, input_lens, label_lens)
+            loss = criterion(output, labels)
 
             if phase == 'train':
                 loss.backward()
                 optimizer.step()
 
+            batch = labels.shape[0]
             total += batch
             total_loss += loss.item()
 
             labels = labels.detach().cpu().numpy()
             labels = [labels[i] for i in range(batch)]
             output = output.detach().cpu().numpy()
-            output = [output[:, i, :] for i in range(batch)]
-            output = [naive_ctc_decode(o) for o in output]
+            # output = [output[:, i, :] for i in range(batch)]
+            # output = [naive_ctc_decode(o) for o in output]
+            output = output.argmax(axis=1)
+            output = [output[i] for i in range(batch)]
             total_acc += sum(np.array_equal(o, l) for o, l in zip(output, labels))
 
     return total_loss / total, total_acc / total
 
 
-def train(train_dir, ext, train_ratio, device, nworkers, max_epochs, checkpoint=None):
+def train(train_dir, ext, train_ratio, device, nworkers, max_epochs, modelname, checkpoint=None):
     train_dir = Path(train_dir)
     assert train_dir.is_dir()
     train_paths = list(train_dir.glob('*.{}'.format(ext)))
     assert len(train_paths) > 0
 
+    label_size = 5
+
     # train set containts only 5-letter labels
-    train_paths = [p for p in train_paths if len(p.stem) == 5]
+    train_paths = [p for p in train_paths if len(p.stem) == label_size]
 
     seed = 1
     random.seed(seed)
@@ -119,10 +126,13 @@ def train(train_dir, ext, train_ratio, device, nworkers, max_epochs, checkpoint=
     train_iter = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=nworkers)
     valid_iter = DataLoader(valid_dataset, batch_size, shuffle=False, num_workers=nworkers)
 
-    model = CaptchaModel(len(train_dataset.symbols)).to(device)
-    criterion = nn.CTCLoss(zero_infinity=True).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=1e-1)
+    # model = CaptchaModel(len(train_dataset.symbols)).to(device)
+    # criterion = nn.CTCLoss(zero_infinity=True).to(device)
+    # optimizer = optim.SGD(model.parameters(), lr=1e-1)
     # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+    model = CaptchaModelFixedSize(len(train_dataset.symbols), label_size).to(device)
+    criterion = nn.NLLLoss().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
     start_epoch = 1
 
     best_loss = 1e6
@@ -139,9 +149,23 @@ def train(train_dir, ext, train_ratio, device, nworkers, max_epochs, checkpoint=
         best_loss = checkpoint['best_loss']
         best_acc = checkpoint['best_acc']
 
+    writer = SummaryWriter('logdir/{}'.format(modelname))
+    sample_input = next(iter(train_iter))
+    sample_images, sample_labels = sample_input
+    for image, label in zip(sample_images, sample_labels):
+        label = ''.join(train_dataset.itos(i.item()) for i in label)
+        writer.add_image('Sample/{}'.format(label), image)
+    with torch.no_grad():
+        writer.add_graph(model, sample_images.to(device))
+    writer.flush()
+
     for epoch in range(start_epoch, max_epochs + 1):
         train_loss, train_acc = run_one_epoch('train', train_iter, model, criterion, optimizer, device)
         valid_loss, valid_acc = run_one_epoch('valid', valid_iter, model, criterion, None, device)
+        writer.add_scalar('Loss/Training', train_loss)
+        writer.add_scalar('Loss/Validation', valid_loss)
+        writer.add_scalar('Accuracy/Training', train_acc)
+        writer.add_scalar('Accuracy/Validation', valid_acc)
         # scheduler.step(valid_loss)
         print('Epoch: {}'.format(epoch))
         print('Train loss: {:.3f}\tTrain acc: {:.3f}'.format(train_loss, train_acc))
@@ -166,16 +190,18 @@ def train(train_dir, ext, train_ratio, device, nworkers, max_epochs, checkpoint=
 
 def launch_train():
     parser = argparse.ArgumentParser()
+    parser.add_argument('modelname')
     parser.add_argument('--train_dir', default='train')
     parser.add_argument('--ext', default='png')
     parser.add_argument('--train_ratio', default=0.8)
     parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str)
-    parser.add_argument('--nworkers', default=4, type=int)
+    parser.add_argument('--nworkers', default=0, type=int)
     parser.add_argument('--max_epochs', default=10000, type=int)
     parser.add_argument('--checkpoint', default=None, type=str)
     args = parser.parse_args()
 
-    train(args.train_dir, args.ext, args.train_ratio, args.device, args.nworkers, args.max_epochs, args.checkpoint)
+    train(args.train_dir, args.ext, args.train_ratio, args.device, args.nworkers, args.max_epochs, args.modelname,
+          args.checkpoint)
 
 
 if __name__ == '__main__':
