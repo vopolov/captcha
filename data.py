30858b7263a7e641ee4704fcaf489f8c4fb2f40c
Vladimir Popov <33720352+vopolov@users.noreply.github.com>
2020-10-11 17:02:50 +0300

    moved transforms outside dataset, which allows for augmentations; moved paths generation outside too; labels are now optional

--- data.py
+++ data.py
@@ -6,44 +6,25 @@ import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
 
 
 class CaptchaDataset(Dataset):
-    def __init__(self, data_dir, device, preload, height, width,
-                 mean=None, std=None, label_size=None, ext='png', mode='rgb'):
+    def __init__(self, paths, with_labels=False, device='cpu', preload=False, transform=None):
         super().__init__()
 
-        self._data_dir = Path(data_dir)
-        assert self._data_dir.is_dir(), '{} is not a valid directory'.format(self._data_dir)
-        self._paths = list(self._data_dir.glob('*.{}'.format(ext)))
-        assert len(self._paths) > 0, 'No files with {} extension found in {}'.format(ext, self._data_dir)
-
+        self._paths = paths
+        self.with_labels = with_labels
         self.device = device
 
-        mode_dict = {'RGB': 'RGB', 'rgb': 'RGB', 'gray': 'L', 'GRAY': 'L', 'L': 'L'}
-        assert mode in mode_dict, '{} mode not supported'.format(mode)
-        self.mode = mode_dict[mode]
-        self.dims = 3 if self.mode == 'RGB' else 1
-
-        self.height = height
-        self.width = width
-
-        self.mean = mean
-        if self.mean:
-            self.mean = torch.tensor(mean).to(device)
-            assert len(self.mean) <= self.dims
-        self.std = std
-        if self.std:
-            self.std = torch.tensor(std).to(device)
-            assert len(self.std) <= self.dims
-
-        self.label_size = label_size
-        if self.label_size is not None:
-            self._paths = [p for p in self._paths if len(p.stem) == self.label_size]
-
         self.symbols = ['-'] + list(ascii_uppercase) + [str(i) for i in range(10)]
-        self._index_dict = {i: s for i, s in enumerate(self.symbols, 0)}  # 0 left for blank predicted symbol
+        self._index_dict = {i: s for i, s in enumerate(self.symbols, 0)}  # 0 left for predicted blank symbol
         self._symbols_dict = {s: i for i, s in enumerate(self.symbols, 0)}
+
+        if transform:
+            self._transform = transform
+        else:
+            self._transform = ToTensor()
 
         self._data = None
         self.preloaded = preload
@@ -63,21 +44,15 @@ class CaptchaDataset(Dataset):
 
     def _read_image(self, path):
         img = Image.open(path)
-        if img.mode != self.mode:
-            img.convert(self.mode)
-        if (self.height is not None and img.size[1] != self.height)\
-                or (self.width is not None and img.size[0] != self.width):
-            img = img.resize((self.width, self.height))
-
-        img = np.array(img)
-        img = torch.tensor(img).permute(2, 0, 1).to(self.device)
-        if self.mean is not None and self.std is not None:
-            img = (img - self.mean[:, None, None]) / self.std[:, None, None]
-
-        label = Path(path).stem.upper()
-        # target dtype must by int32 to work with cudnn ctc loss
-        label = torch.tensor([self.stoi(s) for s in label], dtype=torch.int32).to(self.device)
-        return img, label
+        img = self._transform(img)
+        img = img.to(self.device)
+        if self.with_labels:
+            label = Path(path).stem.upper()
+            # target dtype must by int32 to work with cudnn ctc loss
+            label = torch.tensor([self.stoi(s) for s in label], dtype=torch.int32).to(self.device)
+            return img, label
+        else:
+            return img
 
     def __getitem__(self, index):
         if self.preloaded:
@@ -91,17 +66,12 @@ class CaptchaDataset(Dataset):
 
 if __name__ == '__main__':
     data = Path('train')
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     if data.is_dir():
-        dataset = CaptchaDataset(data, device,
-                                 preload=True,
-                                 height=150,
-                                 width=330,
-                                 mean=(171.28, 181.15, 190.28),
-                                 std=(94.81, 91.23, 83.4),
-                                 label_size=5,
-                                 )
-        image, text = random.choice(dataset)
+        ps = list(data.glob('*.png'))
+        d = 'cuda' if torch.cuda.is_available() else 'cpu'
+        dataset = CaptchaDataset(ps, with_labels=True, preload=True)
+        idx = random.randint(0, len(dataset))
+        image, text = dataset[idx]
         print(len(dataset))
         print(len(dataset.symbols))
 
